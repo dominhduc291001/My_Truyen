a481de963175c5791c0a74ad6530938e5ef10a3e
dominhduc291001 <kesaudoi291001@gmail.com>
2021-05-13 02:39:10 +0700

    update model

--- Data/models.py
+++ Data/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Truyen(models.Model):
+    IDtruyen = models.CharField(max_length = 30, primary_key = True)
+    tentruyen = models.CharField(max_length = 100)
+    tacgia = models.CharField(max_length = 100)
+    nguon = models.CharField(max_length = 100)
+    trangthai = models.BooleanField(default = False)
+    yeuthich = models.IntegerField(default = 0)
+    gioithieu = models.CharField(max_length = 5000 )
+    anh = models.ImageField(upload_to='images/')
+
+class Theloai(models.Model):
+    IDtheloai= models.CharField(max_length=50, primary_key= True)
+    tentheloai =  models.CharField(max_length=100)
+
+class Chaptruyen(models.Model):
+    IDtruyen = models.ForeignKey(Truyen, on_delete=models.CASCADE)
+    chap = models.IntegerField(default=0)
+    tenchap = models.CharField(max_length=100)
+    noidung = RichTextField(blank = True, null= True)
+
+class Checktheloai(models.Model):
+    IDtheloai = models.ForeignKey(Theloai, on_delete=models.CASCADE)
+    IDtruyen = models.ForeignKey(Truyen, on_delete=models.CASCADE)
+
+
+
--- MyTruyen/settings.py
+++ MyTruyen/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Data',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'MyTruyen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MyTruyen',
+        'USER': 'root',
+        'PASSWORD': 'Dmd291001####',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
@@ -126,3 +132,7 @@ STATICFILES_DIRS = [BASE_DIR /'static']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add media ImageField
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
