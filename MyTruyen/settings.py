951542de1c9644e8e820e682f19cba763dd05873
dominhduc291001 <kesaudoi291001@gmail.com>
2021-05-12 17:18:43 +0700

    add static directory

--- MyTruyen/settings.py
+++ MyTruyen/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MyTruyen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR /'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR /'static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
